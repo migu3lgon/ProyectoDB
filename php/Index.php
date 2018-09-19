3275abaff3b24164491d7a53872587bb7861bc6a
marcosmateom <42660787+marcosmateom@users.noreply.github.com>
2018-09-19 16:40:07 -0600

    se creo nav.php y se aplico en informacion.php y index.php ademas se creo la carpeta php para colocar alli los php

---
+++ php/Index.php
@@ -0,0 +1,45 @@
+﻿<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <title>home</title>
+    <style>
+        .navigation_bar {
+            text-align: right;
+            margin-right: 10px;
+        }
+
+        #header_div {
+            padding-top: 10px;
+            background-color: blue;
+        }
+
+        .logo {
+            margin-left: 50px;
+            margin-top: -5px;
+        }
+
+        .navigation_link {
+            color: black;
+            font-weight: bolder;
+        }
+
+        #informacion_e_historia {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <?php include('Nav.php') ?>
+    <a href="informacion.php">link</a>
+
+</body>
+</html>
