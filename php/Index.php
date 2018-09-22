e69e3f5e699abc3e96affb8e65260dfd90ce4296
marcosmateom <42660787+marcosmateom@users.noreply.github.com>
2018-09-22 13:09:24 -0600

    Agrege la nueva NavigationBar, datos de prueba y formato a la pagina de informacion

--- php/Index.php
+++ php/Index.php
@@ -17,5 +17,25 @@
     <?php include('NavigationBar.php') ?>
     <a href="informacion.php">link</a>
 
+    <div class="grid-x">
+      <div class="cell large-8 small-8 medium-6">
+        <h3>Main Column</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+      <div class="cell large-4 small-4 medium-6">
+        <h3>Sidebar</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor</p>
+      </div>
+    </div>
+
+
+
+
+
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/vendor/what-input.js"></script>
+    <script src="js/vendor/foundation.js"></script>
+    <script src="js/app.js"></script>
+
 </body>
 </html>
--- php/informacion.php
+++ php/informacion.php
@@ -1,75 +1,49 @@
-﻿<!DOCTYPE html>
+<!DOCTYPE html>
 
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <!-- Latest compiled JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <title>Informacion Gio's Company</title>
+<meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gio's Company Home</title>
+    <link rel="stylesheet" href="../css/foundation.css">
+    <link rel="stylesheet" href="../css/app.css">
+    <link rel="stylesheet" href="../css/Navigation_Bar.css">
     <style>
-        .navigation_bar {
-            text-align: right;
-            margin-right: 10px;
-        }
-
-        #header_div {
-            padding-top: 10px;
-            background-color: red;
-        }
-		#footer_div{
-            background-color: lightgray;
-        }
-
-        .logo {
-            margin-left: 50px;
-            margin-top: -5px;
-        }
-
-        .navigation_link {
-            color: black;
-            font-weight: bolder;
-        }
-
-        #informacion_e_historia {
-            text-align: center;
-        }
+        
     </style>
 </head>
 <body>
-    <?php include('Nav.php') ?>
-    <h1 id="informacion_e_historia">Informacion e Historia</h1>
-    <div class="row">
-        <div class="col-sm-2"></div>
-        <div class="col-sm-8">
-            <article>
-                <h2>Sobre nosotros:</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipiscing elit arcu condimentum, aenean faucibus hac curabitur eget venenatis dapibus cum, ac molestie purus inceptos erat interdum senectus himenaeos. Montes et commodo nisl pretium a habitasse pharetra nam lacinia, fames dapibus parturient sociosqu purus accumsan sollicitudin quisque facilisi, consequat per ut porta inceptos risus justo ac. Consequat mollis posuere cras ornare primis montes orci eu, augue vel diam semper lobortis dis metus tincidunt, aptent pellentesque taciti tristique duis quisque tempor.</p>
-                <p>Nulla magna ac id metus scelerisque tristique aliquam per, ridiculus nisl vulputate turpis ultrices platea habitasse lobortis, torquent eleifend porta fames penatibus semper hac. Magna ullamcorper sollicitudin aliquet a rhoncus laoreet natoque dis himenaeos purus, orci cubilia per sem id fermentum phasellus commodo pellentesque ut, senectus ornare eros platea sed mattis semper massa tempus. Tempus phasellus vel placerat aptent venenatis mattis cum netus orci, nec justo hac sem erat malesuada donec potenti risus volutpat, nisl integer platea auctor eros purus id blandit.</p>
-                <p>Cum hendrerit primis placerat phasellus ante tellus a urna himenaeos sagittis dictumst, praesent risus vehicula nisi iaculis ac cras nullam nisl duis. Porta venenatis montes libero phasellus scelerisque ac mollis potenti, viverra fringilla ut ad euismod integer pellentesque primis hac, congue nisl maecenas natoque dis urna tempor. Rutrum quisque magna suscipit, gravida.</p>
-            </article>
-        </div>
-        <div class="col-sm-2"></div>
+    <?php include('NavigationBar.php') ?>
+    <h1 class="text-center">Informacion sobre Gio's Company</h1>
+    <article>
+        <div class="grid-container">
+                <div class="grid-x grid-margin grid-padding">
+                    <section>
+                        <div class="cell auto">
+                            <h2>Sobre Nosotros</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                    </section>
+                    <section>
+                        <div class= "cell auto">
+                            <h2>Historia</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas ea iste similique quidem? Labore inventore a magni natus, quo nisi, delectus voluptate at asperiores est, doloribus iusto! Nesciunt, ut voluptatibus!</p>
+                        </div>
+                    </section>
+                </div>
 
-    </div>
-    <div class="row">
-        <div class="col-sm-2"></div>
-        <div class="col-sm-8">
-            <article>
-                <h2>Historia:</h2>
-                <p>Gravida aptent volutpat morbi vestibulum litora massa viverra hendrerit, interdum metus quisque penatibus himenaeos pharetra eu curae erat, a nunc dictumst integer cubilia tellus curabitur. Dui tempus habitant consequat turpis nisi quam commodo, laoreet nisl sapien vulputate id volutpat tortor, nulla pretium curabitur montes phasellus nam. Metus porta sociis nisi dis eu natoque, lectus eget donec consequat vitae ut fermentum, convallis magnis purus mi volutpat.</p>
-                <p>Elementum neque facilisi egestas ligula cubilia suspendisse habitasse, condimentum nam pellentesque quis blandit non, ac hac dictumst penatibus nostra pulvinar. A luctus elementum vulputate diam aenean metus ultrices lacinia suscipit ullamcorper, hendrerit nullam ut laoreet aliquam neque taciti iaculis vehicula. Orci mus convallis torquent proin non ultrices ultricies eget laoreet nam volutpat ridiculus, dapibus aenean sociosqu platea fames consequat tincidunt id litora molestie commodo. Fames accumsan dapibus pharetra diam orci vestibulum mollis inceptos tellus dui semper fermentum habitant, purus erat libero mi nostra sapien consequat metus auctor mattis cursus.</p>
-                <p>Pellentesque urna magna blandit nisl, nulla sagittis sociosqu est dui, rhoncus inceptos varius. Vestibulum laoreet iaculis nascetur facilisi suspendisse, suscipit ullamcorper aenean erat mus hac, hendrerit augue tempor ultricies. Velit faucibus curabitur aenean morbi erat senectus habitant etiam egestas fusce iaculis libero, lectus laoreet nascetur sapien tempor montes justo convallis id vel pellentesque. At potenti taciti eros purus mollis facilisis convallis id nec, et mauris commodo venenatis vestibulum sociosqu eu platea tellus sapien, suscipit ad ut sociis tortor natoque magna sagittis.</p>
-            </article>
         </div>
-        <div class="col-sm-2"></div>
-    </div>
-    <footer>
-		
-    </footer>
+    </article>
+
+
+
+
+
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/vendor/what-input.js"></script>
+    <script src="js/vendor/foundation.js"></script>
+    <script src="js/app.js"></script>
+
 </body>
 </html>
