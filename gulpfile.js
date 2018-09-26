23dfa43fa7b1155dadff9b5b5dd68f1d299641fe
Miguel González <miguelgon_8@hotmail.com>
2018-09-26 12:34:16 -0600

    esta es la prueba 1 de foundation

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,25 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var webserver = require('gulp-webserver');
 
+
 //Var styles para que funcione con el watch
 var Styles_f = function(done) {
-    gulp.src('sass/**/*.scss')
+    gulp.src('src/assets/scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('css/'));
+        .pipe(gulp.dest('src/assets/css/'));
     done();
 };
 
 gulp.task('styles', Styles_f);
 
+gulp.task('default', Styles_f);
+
 //Watch task
 gulp.task('watch', function() {
-    gulp.watch('sass/**/*.scss', Styles_f);
+    gulp.watch('src/assets/scss/**/*.scss', Styles_f);
 });
 
+//pendiente
 gulp.task('webserver', function() {
     gulp.src('app')
         .pipe(webserver({
