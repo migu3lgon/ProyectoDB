960301c1250dc36349524c4b3d15370d264856d4
marcosmateom <42660787+marcosmateom@users.noreply.github.com>
2018-10-08 16:47:07 -0600

    se agregaron los htmls simples para la entrega

---
+++ php/Template.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gio's Company Home</title>
+    <link rel="stylesheet" href="../css/foundation.css">
+    <link rel="stylesheet" href="../css/app.css">
+    <link rel="stylesheet" href="../css/css.css">
+    <script src='../js/vendor/foundation.js'></script>
+</head>
+<body>
+    <?php include('../controladores/navbar_c.php') ?>
+    <div class="mainb">
+
+    </div>
+    <?php include('/partials/Footer.php') ?>
+    
+
+
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/vendor/what-input.js"></script>
+    <script src="js/vendor/foundation.js"></script>
+    <script src="js/app.js"></script>
+
+</body>
+</html>
