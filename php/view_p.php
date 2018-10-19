599e7fa0c6e2ecb7bfeeebe2377d414d12efc3c0
Miguel González <miguelgon_8@hotmail.com>
2018-10-19 17:39:44 -0600

    Funciona view_p

--- php/view_p.php
+++ php/view_p.php
@@ -14,9 +14,21 @@
         $subcat = $_GET['subcat'];
     }
     //querys de anuncios para poblar la pagina
-    $imagen = $conn->query("SELECT * from anuncio where idsubcategoria=$subcat");
+    $con_prod = $conn->query("SELECT idanuncio, idsubcategoria from anuncio where idsubcategoria=$subcat");
+    //arrays de categorias y sub categorias
+    $prod_arr = array();
+
+    //Poblar arrays para mostrar las categorias y sub categorias
+    $j = 0;
+    while ($col2 = mysqli_fetch_array( $con_prod )){
+        $prod_arr[$j] = array($col2[0],$col2[1]);
+        $j = $j + 1;
+    }
+    $count_prod = count($prod_arr);
 ?>
+
 <!DOCTYPE html>
+
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta charset="utf-8">
@@ -34,7 +46,7 @@
         <div class="grid-container">
             <div class="grid-x grid-margin-x grid-margin-y">
                 <?php 
-                    if (!mysqli_fetch_array($imagen)) {
+                    if ($count_prod <= 0) {
                         echo '
                         <div class="callout small-10 medium-10 large-10 align">
                             <h5>Más anuncios proximamente!</h5>
@@ -42,22 +54,25 @@
                         </div>
                         ';
                     }
-                    while($row = mysqli_fetch_array($imagen))  
-                    {  
-                        if ($row['Imagen'] != NULL) {
-                            $img = '<img class="img_anuncio" src="data:image/jpeg;base64,'.base64_encode($row['Imagen'] ).'" width=400  alt="imagen producto"/>';  
-                        }
-                        else {
-                            $img = '<img class="img_anuncio" src="https://placehold.it/180x180" alt="Sin imagen"/>';
-                        }
-                        if ($row['descripcion']!= NULL) {
-                            $prodDesc = $row['descripcion'];
-                        }
-                        else {
-                            $prodDesc = "Sin descripcion.";
-                        }
-                        $prodName = $row['titulo'];
-                        $prodPrice = $row['precio'];
+                    for ($i=0; $i < $count_prod ; $i++) { 
+                        $imagen = $conn->query("SELECT * from anuncio where idanuncio=".$prod_arr[$i][0]." limit 1;");
+                        while($row = mysqli_fetch_array($imagen))  
+                        {  
+                            if ($row['Imagen'] != NULL) {
+                                $img = '<img class="img_anuncio" src="data:image/jpeg;base64,'.base64_encode($row['Imagen'] ).'" width=400  alt="imagen producto"/>';  
+                            }
+                            else {
+                                $img = '<img class="img_anuncio" src="https://placehold.it/180x180" alt="Sin imagen"/>';
+                            }
+                            if ($row['descripcion']!= NULL) {
+                                $prodDesc = $row['descripcion'];
+                            }
+                            else {
+                                $prodDesc = "Sin descripcion.";
+                            }
+                            $prodName = $row['titulo'];
+                            $prodPrice = $row['precio'];
+                        }  
                         echo '
                         <div class="cell small-12 medium-3">
                             <div class="product-card cont">
