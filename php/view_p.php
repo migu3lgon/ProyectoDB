e65ce38f046da9b4a2898b55e967f995217e2709
Miguel González <miguelgon_8@hotmail.com>
2018-10-19 17:05:36 -0600

    subir el nuevo sql

--- php/view_p.php
+++ php/view_p.php
@@ -16,9 +16,7 @@
     //querys de anuncios para poblar la pagina
     $imagen = $conn->query("SELECT * from anuncio where idsubcategoria=$subcat");
 ?>
-
 <!DOCTYPE html>
-
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta charset="utf-8">
