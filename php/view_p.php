d0915a05095eec70ef62be059ad8455339731394
Miguel González <miguelgon_8@hotmail.com>
2018-10-19 16:01:33 -0600

    Cambios en view

--- php/view_p.php
+++ php/view_p.php
@@ -44,7 +44,7 @@
     <?php include('../controladores/navbar_c.php') ?>
     <div class="mainb" align="center">
         <div class="grid-container">
-            <div class="grid-x grid-margin-x grid-margin-y">
+            <div class="grid-x grid-margin-x grid-margin-y align-middle">
                 <?php 
                     for ($i=0; $i < $count_prod ; $i++) { 
                         $imagen = $conn->query("SELECT * from anuncio where idanuncio=".$prod_arr[$i][0]." limit 1;");
