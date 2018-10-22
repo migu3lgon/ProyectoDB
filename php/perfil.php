bd17d2a36b55e51cab5ed323752b710de7a179b6
marcosmateom <42660787+marcosmateom@users.noreply.github.com>
2018-10-21 21:51:35 -0600

    perfil ya funcional usando metodos para la db tambien van 3 sp para que funcionen los querys de esta pagina.

--- php/perfil.php
+++ php/perfil.php
@@ -6,10 +6,14 @@
 
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
+    $conn2 = new mysqli($servername, $username, $password, $dbname);
     // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
-    } 
+    }
+    if ($conn2->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
     
 ?>
 
@@ -20,7 +24,7 @@
 <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gio's Company - Home</title>
+    <title>Perfil</title>
     <link rel="stylesheet" href="../css/foundation.css">
     <link rel="stylesheet" href="../css/app.css">
     <link rel="stylesheet" href="../css/css.css">
@@ -28,8 +32,16 @@
     
 </head>
 <body>
-<?php include('../controladores/navbar_c.php') ?>
-    <div class="mainb" align="center">
+<?php include('../controladores/navbar_c.php');
+    $id = $_SESSION['id_usuario']; 
+    $prueba = $conn2->query("call datos_perfil(".$id.");");
+    $row = $prueba->fetch_assoc(); 
+            $correo = $row["correo"];
+            $nombre = $row["nombre"];
+            $apellido = $row["apellido"];
+            $telefono = $row["telefono"];
+?>
+    <div class="mainb">
     <div class="grid-container">
             <div class="grid-x grid-margin-x">
                 <div class="cell small-12">
@@ -40,15 +52,23 @@
                     </ul>
 
                     <div class="tabs-content" data-tabs-content="example-tabs">
-                        <div class="tabs-panel is-active" id="panel1">
-                            <p>One</p>
-                            <p>Este es el disenio de miguel</p>
+                    <div class="tabs-panel is-active" id="panel1">
+                            <h2>Mi Informacion</h2>
+                            <label>Usuario</label>
+                            <span><?php echo $nombre, " ",  $apellido?></span>
+                            <label>Correo</label>
+                            <span><?php echo $correo ?></span>
+                            <label>Telefono</label>
+                            <span><?php if($telefono){echo $telefono;} else {echo "Aun no has ingresado tu telefono";}; ?></span>
+                            <label>Clave</label>
+                            <span>************</span><a href="cambiar_clave.php"> Cambio de clave</a>
+                            <p><button class="button" data-open="exampleModal1">Actualizar Informacion</button></p>
+                            <p><button class="button" data-open="exampleModal2">Cambiar clave</button></p>
                         </div>
                         <div class="tabs-panel" id="panel2">
                             <div class="grid-container">
-                                <?php
-                                    $id = $_SESSION['id_usuario'];  
-                                        $imagen = $conn->query("SELECT * from anuncio where idusuario=".$id.";");
+                                <?php  
+                                        $imagen = $conn->query("call informacion_mis_anuncios(".$id.");");
                                         
                                         while($row = mysqli_fetch_array($imagen))  
                                         {  
@@ -90,20 +110,6 @@
                                         ';
                                         }  
                                         
-                                        /*<div class="cell small-12 medium-3">
-                                            <div class="product-card cont">
-                                                <div class="product-card-thumbnail anuncio">
-                                                    <a href="#">';echo $img.'</a>
-                                                </div>
-                                                <h2 class="product-card-title cont"><a href="#">';echo $prodName.'</a></h2>
-                                                <span class="product-card-desc">';echo $prodDesc.'</span>
-                                                <br/>
-                                                <span class="product-card-price">Q ';echo $prodPrice.'</span>
-                                                <br/>
-                                                <button class="button">Comprar</button>
-                                                <button class="button">Informacion</button>
-                                            </div>
-                                        </div>*/
                                 ?>
                                 </div>
                                 
@@ -118,6 +124,58 @@
                 </div>
             </div>
         </div>
+
+        <div class="reveal" id="exampleModal1" data-reveal>
+            <form action="perfil.php" method="post" enctype="multipart/form-data">
+                <h2>Actualizacion de perfil</h2>
+                Nombre:<br>
+                <input type="text" name="nombre" value="<?php echo $nombre ?>" placeholder="Ingrese aqui su nombre">
+                Apellido:<br>
+                <input type="text" name="apellido" value="<?php echo $apellido ?>" placeholder="Ingrese aqui su apellido">
+                <br> 
+                Correo Electronico:
+                <br>
+                <input type="text" name="correoe" value="<?php echo $correo ?>" placeholder="Ingrese aqui su correo electronico">
+                <br> 
+                Telefono:
+                <br>
+                <input type="text" name="telefono" value="<?php echo $telefono?>" placeholder="Ingrese aqui su numero de telefono">
+                <br> 
+                clave:
+                <br>
+                <span>************</span> <a href="cambiar_clave.php"> Cambio de clave</a>
+                <input class="button small-12 cell" type="submit" name="actualisar_perfil" value="Actualizar Perfil" />
+            </form>
+            <button class="close-button" data-close aria-label="Close modal" type="button">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <?php
+        /*$prueba = $conn->query("call datos_perfil(".$id.");");
+        $row = $prueba->fetch_assoc(); 
+                $correo = $row["correo"];
+                echo $correo;
+                $nombre = $row["nombre"];
+                echo $nombre;
+                $apellido = $row["apellido"];
+                echo $apellido;
+                $telefono = $row["telefono"];
+                echo $telefono;*/
+
+        if (isset($_POST["actualisar_perfil"])) {
+            $correo2 = $_POST["correoe"];
+            $nombre2 = $_POST["nombre"];
+            $apellido2 = $_POST["apellido"];
+            $telefono2 = $_POST["telefono"];
+            $cons2 = $conn->query("CALL modificar_perfil(".$id.",'".$correo2."','".$nombre2."','".$apellido2."',".$telefono2.");");
+            if ($cons2){
+                echo "si funciono";
+            } else{
+                echo "Problema con el sp";
+            }
+        }
+        ?>
+
     </div>
 
     <?php include('/partials/Footer.php') ?>
