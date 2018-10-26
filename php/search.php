a78a897c1a481fab2859b4cc33331455b118aa19
manupianista <manuelecastilloo@gmail.com>
2018-10-25 18:48:08 -0600

    errores corregidos

--- php/search.php
+++ php/search.php
@@ -88,15 +88,18 @@
                     if(isset($_GET['subcategoria'])){
                     $valSub = $_GET['subcategoria'];
                     $catglo = $valSub;
-                    //echo "cat value: ".$catglo."<br>";}
+                    //echo "cat value: ".$catglo."<br>";
+                }
                     if(isset($_GET['ubicacion'])){
                     $valLoc = $_GET['ubicacion'];
                     $locglo = $valLoc;
-                    //echo "locat value: ".$valLoc."<br>";}
+                    //echo "locat value: ".$valLoc."<br>";
+                }
                     if(isset($_GET['precio'])){
                     $valPrec = $_GET['precio'];
                     $precglo = $valPrec;
-                    //echo "price value: ".$valPrec;}
+                    //echo "price value: ".$valPrec;
+                }
                     
 
                     echo '
