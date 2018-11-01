ee2bfb00f8c09cac688cbe9b957a6e01076474da
manupianista <manuelecastilloo@gmail.com>
2018-10-31 18:47:50 -0600

    Commit de algo

--- php/search.php
+++ php/search.php
@@ -32,7 +32,7 @@
     $cat_arr = array();
     $subcat_arr = array();
     $ubic_arr = array();
-   
+    $hglobal;
 
     //Poblar arrays para mostrar las categorias y sub categorias
     $i = 0; $j = 0; $h = 0;
@@ -47,6 +47,7 @@
     while ($col3 = mysqli_fetch_array( $con_ubic )){
         $ubic_arr[$h] = array($col3[0],$col3[1]);
         $h = $h + 1;
+        $hglobal=$h;
     }
     
     $count_cat = count($cat_arr);
@@ -58,6 +59,39 @@
 <!DOCTYPE html>
 
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<script>
+    $(document).ready(function(){
+        $.ajax({
+                type:'GET',
+                url:'../jsons/subcat_json.php',
+                dataType: "json",
+                success:function(data){
+                    var $subc = $('#subc');
+                    $subc.empty();
+                    for (var a = 0; a < data['cat'].length; a++) {
+                        $subc.append('<optgroup label=' + data['cat'][a][1] + '/>')
+                        for (var i = 0; i < data['subcat'].length; i++) {
+                            if (data['cat'][a][0]==data['subcat'][i][1]) {
+                                $subc.append('<option value='+ data['subcat'][i][0] + '>'+ data['subcat'][i][2] +'</option>');   
+                            }                        
+                        }
+                    }
+                }
+            });
+        $.ajax({
+            type:'GET',
+            url:'../jsons/ubic_json.php',
+            dataType: "json",
+            success:function(data2){
+                var $ubic = $('#ubic');
+                $ubic.empty();
+                for (var l=0; l < data2.length; l++) { 
+                    $ubic.append('<option value=' + data2[l][0] + '>' + data2[l][1] + '</option>');
+                }
+            }
+        });
+    });
+    </script>
 <head>
 <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
@@ -78,6 +112,7 @@
         $valorglo;
         $catglo;
         $locglo;
+        $locglo2;
         $precglo;
                 {
                 if(isset($_GET['search'])){
@@ -85,94 +120,122 @@
                     $valorglo = $_GET['search'];
                     //echo "search value: ".$valorglo."<br>"; 
                     $value=$_GET['search'];
-                    if(isset($_GET['subcategoria'])){
-                    $valSub = $_GET['subcategoria'];
-                    $catglo = $valSub;
-                    //echo "cat value: ".$catglo."<br>";
-                }
-                    if(isset($_GET['ubicacion'])){
-                    $valLoc = $_GET['ubicacion'];
-                    $locglo = $valLoc;
-                    //echo "locat value: ".$valLoc."<br>";
-                }
-                    if(isset($_GET['precio'])){
-                    $valPrec = $_GET['precio'];
-                    $precglo = $valPrec;
-                    //echo "price value: ".$valPrec;
-                }
+                    {
+                        if(isset($_GET['subcategoria'])){
+                        $catglo = $_GET['subcategoria'];
+                        //$catglo = $valSub;
+                        echo "cat value: ".$catglo."<br>";
+                        }
+                        else {
+                            $catglo=NULL;
+                        }
+                    }
+                    {
+                        if(isset($_GET['ubicacion'])){
+                        $locglo = $_GET['ubicacion'];
+                        //$locglo = $valLoc;
+                        echo "locat value: ".$locglo."<br>";
+                        
+                        }
+                        else {
+                            $locglo=NULL;
+                        }
+                    }
+                    {
+                        if(isset($_GET['precio'])){
+                        $precglo = $_GET['precio'];
+                        //$precglo = $valPrec;
+                        echo "price value: ".$precglo;
+                        }
+                        else {
+                            $precglo=NULL;
+                        }
+                    }
                     
 
                     echo '
                     <form action="search.php" method="get">
                     <input type="hidden" name="search" value="'.$valorglo.'">';
+                    /*echo '<select id="subc" name="subcategoriaa">
+                    </select>';
+                    echo "<select id='ubic'";
+                    echo 'name="ubicaciona">
+                    </select>';*/
                     echo '
                         <select name="subcategoria">
-                        <option value="'.$catglo.'" disabled selected>Categorias</option>'
-                        ;
-                            
+                        <option value="'.$catglo.'" disabled selected>';
+                        echo '</option>';
+                                echo '<option value="'.$catglo.'" selected>Categoria: '.$catglo.'</option>';
                                 for ($i=0; $i < $count_cat ; $i++) { 
                                     echo "<optgroup label=".$cat_arr[$i][1].">";
                                     for ($k=0; $k < $count_subcat; $k++) { 
                                         if ($cat_arr[$i][0]==$subcat_arr[$k][1]) {
-                                            echo "<option value=".$subcat_arr[$k][2].">".$subcat_arr[$k][2]."</option>";
+                                            echo '<option value="'.$subcat_arr[$k][2].'">'.$subcat_arr[$k][2].'</option>';
                                         }
                                     }
                                 }
                            
                         echo '</select>';
                     echo '<select name="ubicacion">
-                    <option value="'.$locglo.'" disabled selected>Ubicacion</option>';
-                        
-                            for ($l=0; $l < $count_subcat; $l++) { 
-                                echo "<option value=".$ubic_arr[$l][1].">".$ubic_arr[$l][1]."</option>";
+                    <option value="'.$locglo.'" disabled selected>';if($locglo==NULL){
+                        echo 'Ubicación';
+                    } else {echo $locglo;}
+                    echo '</option>';
+                            echo '<option value="'.$locglo.'" selected>Ubicacion: '.$locglo.'</option>';
+                            for ($l=0; $l < $hglobal; $l++) { 
+                                echo '<option value="'.$ubic_arr[$l][0].'">'.$ubic_arr[$l][1].'</option>';
                             }
                         
                     echo '</select>';
+                    /*if($locglo2!=NULL){
+                    echo '<input type="hidden" name="subcategoria" value="'.$locglo2.'">';}*/
                     echo '
                     Precio < 
-                    <input type="text" name="precio" value="">';
+                    <input type="text" name="precio" value="'.$precglo.'">';
                     
                                    
 
                     echo '<input type="submit" value="Submit">
                     </form>
-                    ';
                     
+                    ';
 
                     echo '<div class="grid-x grid-margin-x grid-margin-y">';
-                    if(isset($_GET['subcategoria'])){
-                    $sql = "CALL getData('$value','$catglo','','')";
-                    }
-                    else if(isset($_GET['ubicacion'])){
-                        $sql = "CALL getData('$value','','$locglo','')";
-                    }
-                    else if(isset($_GET['precio'])){
-                        $sql = "CALL getData('$value','','','$precglo')";
-                    }
-                    else if(isset($_GET['subcategoria'],$_GET['precio'])){
-                        $sql = "CALL getData('$value','$catglo','','$precglo')";
-                    } 
-                    else if(isset($_GET['ubicacion'],$_GET['subcategoria'])){
-                        $sql = "CALL getData('$value','$catglo','$locglo','')";
-                    }
-                    else if(isset($_GET['ubicacion'],$_GET['precio'])){
-                        $sql = "CALL getData('$value','','$locglo','$precglo')";
-                    }
-                    else if(isset($_GET['ubicacion'],$_GET['subcategoria'],$_GET['precio'])){
-                        $sql = "CALL getData('$value','$catglo','$locglo','$precglo')";
-                    } 
-                    else {
-                        $sql = "CALL getData('$value','','','')";
-                    }
+                    if(isset($_GET['search'])){
+                    switch($_GET['search']){
+                        case isset($_GET['subcategoria']):
+                            $sql = "CALL getData('$value','$catglo','','')";
+                            break;
+                        case isset($_GET['ubicacion']):
+                            $sql = "CALL getData('$value','','$locglo','')";
+                            break;
+                        case isset($_GET['precio']):
+                            $sql = "CALL getData('$value','','','$precglo')";
+                            break;
+                        case isset($_GET['subcategoria'],$_GET['precio']):
+                            $sql = "CALL getData('$value','$catglo','','$precglo')";
+                            break;
+                        case isset($_GET['ubicacion'],$_GET['subcategoria']):
+                            $sql = "CALL getData('$value','$catglo','$locglo','')";
+                            break;
+                        case isset($_GET['ubicacion'],$_GET['precio']):
+                            $sql = "CALL getData('$value','','$locglo','$precglo')";
+                            break;
+                        case isset($_GET['ubicacion'],$_GET['subcategoria'],$_GET['precio']):
+                            $sql = "CALL getData('$value','$catglo','$locglo','$precglo')";
+                            break;
+                        case isset($_GET['search']):
+                            $sql = "CALL getData('$value','','','')";
+                            break;
+
+
+                    }}
+
+
+
+                    
                     $result=mysqli_query($conn, $sql);
-                    /*$sql3 = "CALL getData('$value','','','')";
-                    $result3=mysqli_query($conn, $sql3);*/
-                    /*
-                    $valorglo;
-                    $catglo;
-                    $locglo;
-                    $precglo;
-                      */
+                    
 
                     while($row=mysqli_fetch_array($result)){
                         $prodId = $row['idanuncio'];
@@ -237,6 +300,7 @@
     <script src="js/vendor/what-input.js"></script>
     <script src="js/vendor/foundation.js"></script>
     <script src="js/app.js"></script>
+    
 
 </body>
 </html>
