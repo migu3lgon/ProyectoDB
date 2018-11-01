1e5a17ef32f1a5c02cdd3e63f8496817844ba446
Miguel González <miguelgon_8@hotmail.com>
2018-11-01 11:09:23 -0600

    cat.js

--- php/partials/navbar_logged.php
+++ php/partials/navbar_logged.php
@@ -1,22 +1,7 @@
 <?php
   $usuario = $_SESSION['username'];
 ?>
-<script>
-  $(document).ready(function(){
-    $.ajax({
-          type:'POST',
-          url:'../jsons/cat_json.php',
-          dataType: "json",
-          success:function(data){
-              var $cat = $('#cat');
-              $cat.empty();
-              for (var i=0; i < data.length ; i++) { 
-                $cat.append("<li><a href=\"cat.php?cat=" + data[i][0] + "\">" + data[i][1] + "</a></li>");
-              }
-          }
-      });
-  });
-</script>
+<script src='../js/cat.js'></script>
 
 <!--logged navbar-->
 <header>
